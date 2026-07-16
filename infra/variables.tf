9093bd2d67dd7f455661d872b63e789ceef7ff97
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-07-16 15:48:01 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -3,7 +3,6 @@ variable "environment" {
   type        = string
   default     = "dev"
 
-
   validation {
     condition     = contains(["dev", "staging", "prod"], var.environment)
     error_message = "environment must be one of: dev, staging, prod."
