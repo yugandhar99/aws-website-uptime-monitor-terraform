eb53bc81b82f9ade15bbcd7cc92b6c21e069248b
Ethamukkala Yugandhar <104614963+yugandhar99@users.noreply.github.com>
2026-09-17 14:14:14 -0500

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -4,7 +4,7 @@
 #   backend "s3" {
 #     bucket         = "your-terraform-state-bucket"
 #     key            = "website-uptime-monitor/terraform.tfstate"
-#     region         = "us-west-2" 
+#     region         = "us-west-2"
 #     dynamodb_table = "your-terraform-lock-table"
 #     encrypt        = true
 #   }
